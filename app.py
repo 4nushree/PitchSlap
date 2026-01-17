40bf08d839c59864f7b3fd09e647034f87206b9c
Anushree Bobade <anushreework21@gmail.com>
2026-01-17 11:37:56 +0530

    basic streamlit UI test

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="PitchSlap", layout="centered")
+
+st.title("🔥 PitchSlap")
+st.subheader("AI Startup Idea Validator")
+
+st.write("If you see this, Streamlit is WORKING 🥳")
